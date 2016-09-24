852f81eaa3b9c9e1f91a3759a9dfb95032e22e8b
Cesar De La Cruz <cesardelacruz@Cesars-MacBook-Pro.local>
2016-09-23 23:25:52 -0400

    testing

--- server.js
+++ server.js
@@ -41,37 +41,54 @@ app.get("/", function (request, response) {
 
 app.post('/', function(req, res) {
   if (req.body.text == '/scores') {    
-    //var botResponse;
-    botResponse = getScores();
+    // Get scores
+    url = "http://games.espn.com/ffl/scoreboard?leagueId=560005&seasonId=2016";
+    var botResponse = "";
     
-    var options, body, botReq;    
-    options = {
-      hostname: 'api.groupme.com',
-      path: '/v3/bots/post',
-      method: 'POST'
-    };
-    body = {
-      "bot_id" : botID,
-      "text" : botResponse
-    };
-    //console.log('sending ' + botResponse + ' to ' + botID);
-    botReq = HTTPS.request(options, function(res) {
-        if(res.statusCode == 202) {
-          //neat
-          console.log('sent');
-        } else {
-          console.log('rejecting bad status code ' + res.statusCode);
-        }
+    request(url, function(error, response, html) {
+      var $ = cheerio.load(html);
+      $('.ptsBased').each(function(i, el) {      
+        var name1 = $(this).children().eq(0).children().children('.name').children().eq(0).text();
+        var record1 = $(this).children().eq(0).children().children().eq(1).children('.record').text();
+        var score1 = $(this).children().eq(0).children('.score').text();
+        var proj1 = $(this).children().eq(2).children().children('.scoringDetails').children().eq(2).children().eq(3).text();
+        var name2 = $(this).children().eq(1).children().children('.name').children().eq(0).text();
+        var record2 = $(this).children().eq(1).children().children().eq(1).children('.record').text();
+        var score2 = $(this).children().eq(1).children('.score').text();
+        var proj2 = $(this).children().eq(2).children().children('.scoringDetails').children().eq(5).children().eq(3).text();
+        botResponse += name1 + " "+ record1 +": " + score1 + " ("+proj1+") \r\n vs \r\n" + name2 + " " + record2 +": " + score2 + " ("+proj2+")";
+        botResponse += "\n---------------\n";
+
+      });
+
+      var options, body, botReq;    
+      options = {
+        hostname: 'api.groupme.com',
+        path: '/v3/bots/post',
+        method: 'POST'
+      };
+      body = {
+        "bot_id" : botID,
+        "text" : botResponse
+      };
+      //console.log('sending ' + botResponse + ' to ' + botID);
+      botReq = HTTPS.request(options, function(res) {
+          if(res.statusCode == 202) {
+            //neat
+            console.log('sent');
+          } else {
+            console.log('rejecting bad status code ' + res.statusCode);
+          }
+      });
+      botReq.on('error', function(err) {
+        console.log('error posting message '  + JSON.stringify(err));
+      });
+      botReq.on('timeout', function(err) {
+        console.log('timeout posting message '  + JSON.stringify(err));
+      });
+      botReq.end(JSON.stringify(body));
     });
-    botReq.on('error', function(err) {
-      console.log('error posting message '  + JSON.stringify(err));
-    });
-    botReq.on('timeout', function(err) {
-      console.log('timeout posting message '  + JSON.stringify(err));
-    });
-    botReq.end(JSON.stringify(body));
-  }
-  
+  };
   
 });
 
